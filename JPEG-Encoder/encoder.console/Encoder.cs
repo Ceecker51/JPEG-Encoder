843582ef502047cc70cb5a9689c202dcf36b341c
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-10-09 00:03:09 +0200

    rename console app file and add lib dependency

---
+++ JPEG-Encoder/encoder.console/Encoder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using encoder.lib;
+
+namespace encoder.console
+{
+  class Encoder
+  {
+    static void Main(string[] args)
+    {
+      Picture picture = PPMReader.ReadFromPPMFile(@"C:\Users\willi\Documents\Multimedia II\JPEG-Encoder\JPEG-Encoder\encoder.console\test.ppm");
+
+      for (int y = 0; y < picture.Height; y++)
+      {
+        for (int x = 0; x < picture.Width; x++)
+        {
+          Pixel pixel = picture.GetPixel(x, y);
+          Console.Write(pixel);
+          Console.WriteLine();
+        }
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
