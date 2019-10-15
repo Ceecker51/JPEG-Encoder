4d879da43fc61da03c44a05a124b2274e8389b16
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-10-15 21:29:33 +0200

    remove command line parameter

--- JPEG-Encoder/encoder.console/Encoder.cs
+++ JPEG-Encoder/encoder.console/Encoder.cs
@@ -8,19 +8,12 @@ namespace encoder.console
   {
     private const int stepX = 8;
     private const int stepY = 8;
+    private const bool isWindows = true;
 
     static void Main(string[] args)
     {
-      // check for operation system
-      bool isWindows = true;
-      if (args.Length > 0)
-      {
-        isWindows = !args[0].ToString().Equals("-mac");
-      }
-
       string fileName = "test_5x5.ppm";
       string filePath = isWindows ? @"../../../../images/" + fileName : @"../images/" + fileName;
-
 
       Picture rgbPicture = PPMReader.ReadFromPPMFile(filePath, stepX, stepY, isWindows);
       rgbPicture.Print();
@@ -28,11 +21,11 @@ namespace encoder.console
       Picture yCbCrPicture = Picture.toYCbCr(rgbPicture);
 
       yCbCrPicture.Print();
+
       yCbCrPicture.ReduceY(2);
       yCbCrPicture.ReduceCb(4);
       yCbCrPicture.ReduceCr(8);
       yCbCrPicture.Print();
-
     }
   }
 }
