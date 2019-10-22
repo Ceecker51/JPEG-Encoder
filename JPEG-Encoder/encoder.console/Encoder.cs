ff4fed7d46e2f81ff3d1c9906024ef07eb1335b7
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-10-22 13:09:53 +0200

    read from file stream

--- JPEG-Encoder/encoder.console/Encoder.cs
+++ JPEG-Encoder/encoder.console/Encoder.cs
@@ -10,12 +10,12 @@ namespace encoder.console
   {
     private const int stepX = 8;
     private const int stepY = 8;
-    private const bool isWindows = true;
+    private const bool isWindows = false;
 
     static void Main(string[] args)
     {
-      // string inputFilename = "triumphant.ppm";
-      // string inputFilePath = isWindows ? @"../../../../assets/" + inputFilename : @"../assets/" + inputFilename;
+      string inputFilename = "test.txt";
+      string inputFilePath = isWindows ? @"../../../../assets/" + inputFilename : @"../assets/" + inputFilename;
 
       // Picture rgbPicture = PPMReader.ReadFromPPMFile(inputFilePath, stepX, stepY);
       // rgbPicture.Print();
@@ -33,10 +33,10 @@ namespace encoder.console
       // yCbCrPicture.ReduceCr(8);
 
       // yCbCrPicture.Print();
-      bitStreamStuff();
+      bitStreamStuffFileStream(inputFilePath);
     }
 
-    public static void bitStreamStuff()
+    public static void bitStreamStuffMemoryStream()
     {
       UnicodeEncoding uniEncoding = new UnicodeEncoding();
       byte[] testString = uniEncoding.GetBytes("b");
@@ -44,19 +44,25 @@ namespace encoder.console
       byte[] euro = uniEncoding.GetBytes(
             "€");
 
-
       using (MemoryStream memStream = new MemoryStream(100))
       {
         memStream.Write(testString, 0, testString.Length);
-
         memStream.Seek(0, SeekOrigin.Begin);
-
         var bitStream = new BitStream(memStream);
-
         bitStream.prettyPrint();
-
       }
     }
+
+    public static void bitStreamStuffFileStream(string fileName)
+    {
+
+      using (FileStream fileStream = new FileStream(fileName, FileMode.Open))
+      {
+        var bitStream = new BitStream(fileStream);
+        bitStream.prettyPrint();
+      }
+    }
+
 
   }
 
