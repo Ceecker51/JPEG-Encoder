724cf89388e0e7f3e39a94bd4d4f0d9de35fb39c
Marco Goebel <marco.goebel.1@student.fhws.de>
2019-10-30 17:44:50 +0100

    move segment writting into JPEGWriter class

--- JPEG-Encoder/encoder.console/Encoder.cs
+++ JPEG-Encoder/encoder.console/Encoder.cs
@@ -9,7 +9,7 @@ namespace encoder.console
   {
     private const int stepX = 8;
     private const int stepY = 8;
-    private const bool isWindows = false;
+    private const bool isWindows = true;
 
     static void Main(string[] args)
     {
@@ -18,104 +18,15 @@ namespace encoder.console
       writeJPEGHeader("test_5x5.ppm", "output_5x5.jpg");
     }
 
-    public static void writeJPEGHeader(string inputFileName, string outputFileName)
+    public static void writeJPEGHeader(string ppmFileName, string jpegFileName)
     {
-      string inputFilePath = isWindows ? @"../../../../assets/" + inputFileName : @"../assets/" + inputFileName;
-      string outputFilePath = isWindows ? @"../../../../assets/out_" + outputFileName : @"../assets/out_" + outputFileName;
+      string inputFilePath = isWindows ? @"../../../../assets/" + ppmFileName : @"../assets/" + ppmFileName;
+      string outputFilePath = isWindows ? @"../../../../assets/out_" + jpegFileName : @"../assets/out_" + jpegFileName;
 
       Picture rgbPicture = PPMReader.ReadFromPPMFile(inputFilePath, stepX, stepY);
       Picture yCbCrPicture = Picture.toYCbCr(rgbPicture);
 
-      // Write JPEG header to BitStream
-      BitStream jpegStream = new BitStream();
-
-      // Write SOI-Segment
-      UInt16 startOfImage = 0xFFD8;
-      jpegStream.writeHex(startOfImage);
-
-      // Write APP0-Segment
-      writeAPP0Segment(jpegStream);
-      writeSOF0Segment(jpegStream, Convert.ToUInt16(yCbCrPicture.Height), Convert.ToUInt16(yCbCrPicture.Width));
-
-      // Write to file
-      writeToFile(jpegStream, outputFilePath);
-    }
-
-    public static void writeAPP0Segment(BitStream bitStream)
-    {
-      // APP0 segment
-      UInt16 startMarker = 0xFFE0;
-      UInt16 lengthOfSegment = 16; //length = 16
-      byte versionhi = 1;
-      byte versionlo = 1;
-      byte xyunits = 0;   // 0 = no units, normal density
-      UInt16 xdensity = 1;  // 1
-      UInt16 ydensity = 1;  // 1
-      byte thumbnwidth = 0; // 0
-      byte thumbnheight = 0; // 0
-
-      //Write APP0 section
-      bitStream.writeHex(startMarker);
-      bitStream.writeHex(lengthOfSegment);
-      bitStream.writeByte((byte)'J');
-      bitStream.writeByte((byte)'F');
-      bitStream.writeByte((byte)'I');
-      bitStream.writeByte((byte)'F');
-      bitStream.writeByte(0x0);
-      bitStream.writeByte(versionhi);
-      bitStream.writeByte(versionlo);
-      bitStream.writeByte(xyunits);
-      bitStream.writeHex(xdensity);
-      bitStream.writeHex(ydensity);
-      bitStream.writeByte(thumbnheight);
-      bitStream.writeByte(thumbnwidth);
-    }
-
-    public static void writeSOF0Segment(BitStream bitStream, UInt16 ht, UInt16 wid)
-    {
-      // SOF0 Segment
-      UInt16 marker = 0xFFC0;
-      UInt16 length = 17;
-      byte precision = 8;
-      byte nrofcomponents = 3;
-
-      byte IdY = 1;
-      byte QTY = 0;
-      byte HVY = 0x11;
-
-      byte IdCb = 2; // = 2
-      byte HVCb = 0x11;
-      byte QTCb = 1; // 1
-
-      byte IdCr = 3; // = 3
-      byte HVCr = 0x11;
-      byte QTCr = 1;
-
-      // Write SOF0 segment
-      bitStream.writeHex(marker);
-      bitStream.writeHex(length);
-      bitStream.writeByte(precision);
-      bitStream.writeHex(ht);
-      bitStream.writeHex(wid);
-      bitStream.writeByte(nrofcomponents);
-      bitStream.writeByte(IdY);
-      bitStream.writeByte(HVY);
-      bitStream.writeByte(QTY);
-      bitStream.writeByte(IdCb);
-      bitStream.writeByte(HVCb);
-      bitStream.writeByte(QTCb);
-      bitStream.writeByte(IdCr);
-      bitStream.writeByte(HVCr);
-      bitStream.writeByte(QTCr);
-    }
-
-    public static void writeToFile(BitStream bitStream, string outputFilePath)
-    {
-
-      using (FileStream outputFileStream = new FileStream(outputFilePath, FileMode.Create))
-      {
-        bitStream.writeToStream(outputFileStream);
-      }
+      JPEGWriter.WritePictureToJPEG(outputFilePath, yCbCrPicture);
     }
 
     public static void writeFromBitStreamToFile(string outputFilename)
@@ -143,10 +54,9 @@ namespace encoder.console
 
     public static void readFromFileStreamAndWriteToFile(string outputFilename)
     {
-
       string inputFilename = "test.txt";
-      string inputFilePath = isWindows ? @"../../../../assets/" + inputFilename : @"../assets/" + inputFilename;
 
+      string inputFilePath = isWindows ? @"../../../../assets/" + inputFilename : @"../assets/" + inputFilename;
       string outputFilePath = isWindows ? @"../../../../assets/" + outputFilename : @"../assets/" + outputFilename;
 
       BitStream bitStream = new BitStream();
--- JPEG-Encoder/encoder.console/lib/JPEGWriter.cs
+++ JPEG-Encoder/encoder.console/lib/JPEGWriter.cs
@@ -1,8 +1,108 @@
+using System;
+using System.IO;
+
 namespace encoder.lib
 {
   public class JPEGWriter
   {
+    public static void WritePictureToJPEG(string file, Picture picture)
+    {
+      BitStream jpegStream = new BitStream();
 
+      // Write header segements
+      writeSOISegment(jpegStream);
+      writeAPP0Segment(jpegStream);
+      writeSOF0Segment(jpegStream, Convert.ToUInt16(picture.Height), Convert.ToUInt16(picture.Width));
+      writeEOISegment(jpegStream);
 
+      // Write to file
+      writeToFile(jpegStream, file);
+    }
+
+    private static void writeSOISegment(BitStream jpegStream)
+    {
+      UInt16 startOfImage = 0xFFD8;
+      jpegStream.writeHex(startOfImage);
+    }
+
+    private static void writeEOISegment(BitStream jpegStream)
+    {
+      UInt16 endOfImage = 0xFFD9;
+      jpegStream.writeHex(endOfImage);
+    }
+
+    private static void writeAPP0Segment(BitStream bitStream)
+    {
+      UInt16 startMarker = 0xFFE0;
+      UInt16 lengthOfSegment = 16; //length = 16
+      byte versionhi = 1;
+      byte versionlo = 1;
+      byte xyunits = 0;   // 0 = no units, normal density
+      UInt16 xdensity = 1;  // 1
+      UInt16 ydensity = 1;  // 1
+      byte thumbnwidth = 0; // 0
+      byte thumbnheight = 0; // 0
+
+      //Write APP0 section
+      bitStream.writeHex(startMarker);
+      bitStream.writeHex(lengthOfSegment);
+      bitStream.writeByte((byte)'J');
+      bitStream.writeByte((byte)'F');
+      bitStream.writeByte((byte)'I');
+      bitStream.writeByte((byte)'F');
+      bitStream.writeByte(0x0);
+      bitStream.writeByte(versionhi);
+      bitStream.writeByte(versionlo);
+      bitStream.writeByte(xyunits);
+      bitStream.writeHex(xdensity);
+      bitStream.writeHex(ydensity);
+      bitStream.writeByte(thumbnheight);
+      bitStream.writeByte(thumbnwidth);
+    }
+
+    private static void writeSOF0Segment(BitStream bitStream, UInt16 ht, UInt16 wid)
+    {
+      UInt16 marker = 0xFFC0;
+      UInt16 length = 17;
+      byte precision = 8;
+      byte nrofcomponents = 3;
+
+      byte IdY = 1;
+      byte QTY = 0;
+      byte HVY = 0x11;
+
+      byte IdCb = 2; // = 2
+      byte HVCb = 0x11;
+      byte QTCb = 1; // 1
+
+      byte IdCr = 3; // = 3
+      byte HVCr = 0x11;
+      byte QTCr = 1;
+
+      // Write SOF0 segment
+      bitStream.writeHex(marker);
+      bitStream.writeHex(length);
+      bitStream.writeByte(precision);
+      bitStream.writeHex(ht);
+      bitStream.writeHex(wid);
+      bitStream.writeByte(nrofcomponents);
+      bitStream.writeByte(IdY);
+      bitStream.writeByte(HVY);
+      bitStream.writeByte(QTY);
+      bitStream.writeByte(IdCb);
+      bitStream.writeByte(HVCb);
+      bitStream.writeByte(QTCb);
+      bitStream.writeByte(IdCr);
+      bitStream.writeByte(HVCr);
+      bitStream.writeByte(QTCr);
+    }
+
+    private static void writeToFile(BitStream bitStream, string outputFilePath)
+    {
+      using (FileStream outputFileStream = new FileStream(outputFilePath, FileMode.Create))
+      {
+        bitStream.writeToStream(outputFileStream);
+      }
+    }
   }
 }
